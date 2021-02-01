86b05a1b88c4109ce9bb383d5c24eaed695c2666
Agar <47314805+akaAgar@users.noreply.github.com>
2021-02-01 19:24:11 +0100

    Helicopter now goes to the landing zone as soon as the marker is added on the F10 map

--- Include/Lua/IncludedScripts/FriendlyTransportHelicopterMenuCommand.lua
+++ Include/Lua/IncludedScripts/FriendlyTransportHelicopterMenuCommand.lua
@@ -1,7 +1,8 @@
 briefingRoom.mission.features.friendlyTransportHelicopter = { }
-briefingRoom.mission.features.friendlyTransportHelicopter.activated = false -- was the transport helo activated?
 briefingRoom.mission.features.friendlyTransportHelicopter.MARKER_NAME = "helicopter"
+briefingRoom.mission.features.friendlyTransportHelicopter.activated = false -- was the transport helo activated?
 briefingRoom.mission.features.friendlyTransportHelicopter.markID = nil -- ID of the mark on the map
+briefingRoom.mission.features.friendlyTransportHelicopter.disableCooRemovedRadioMessage = false
 
 -- Activate aircraft and launch bombing run
 function briefingRoom.mission.features.friendlyTransportHelicopter.goToCoordinates()
@@ -10,12 +11,26 @@ function briefingRoom.mission.features.friendlyTransportHelicopter.goToCoordinat
   local transportHeloGroup = dcsExtensions.getGroupByID(briefingRoom.mission.featuresUnitGroups.FriendlyTransportHelicopter[1])
   if transportHeloGroup == nil then return end -- no group (destroyed? missing?), no radio answer
 
+  local destination = nil
+  local marks = world.getMarkPanels()
+  for _,m in ipairs(marks) do
+    if briefingRoom.mission.features.friendlyTransportHelicopter.markID ~= nil and m.idx == briefingRoom.mission.features.friendlyTransportHelicopter.markID then
+      destination = m.pos
+    end
+  end
+
+  if destination == nil then return end -- no coordinates, nothing to do
+
   briefingRoom.radioManager.play("Copy, on my way to the LZ.", "RadioOtherPilotOnMyWayToLZ", briefingRoom.radioManager.getAnswerDelay())
 
   if not briefingRoom.mission.features.friendlyTransportHelicopter.activated then
     transportHeloGroup:activate()
     briefingRoom.mission.features.friendlyTransportHelicopter.activated = true
   end
+
+  local ctrl = transportHeloGroup:getController()
+  local landTask = { id= 'Land', params = { point = destination, durationFlag = false, duration = 3600 } }
+  ctrl:setTask(landTask)
 end
 
 function briefingRoom.mission.features.friendlyTransportHelicopter:onEvent(event)
@@ -35,22 +50,23 @@ function briefingRoom.mission.features.friendlyTransportHelicopter:onEvent(event
         briefingRoom.mission.features.friendlyTransportHelicopter.disableCooRemovedRadioMessage = false
       end
       briefingRoom.mission.features.friendlyTransportHelicopter.markID = event.idx
-      briefingRoom.radioManager.play("Copy, coordinates updated.", "RadioCoordinatesUpdatedF")
+      briefingRoom.mission.features.friendlyTransportHelicopter.goToCoordinates()
       return
     end
   elseif event.id == world.event.S_EVENT_MARK_CHANGE then
     local markText = string.lower(tostring(event.text or ""))
 
     if markText == briefingRoom.mission.features.friendlyTransportHelicopter.MARKER_NAME then
-      briefingRoom.radioManager.play("Copy, coordinates updated.", "RadioCoordinatesUpdatedF")
+      --briefingRoom.radioManager.play("Copy, coordinates updated.", "RadioCoordinatesUpdatedF")
       if briefingRoom.mission.features.friendlyTransportHelicopter.markID ~= nil then
         briefingRoom.mission.features.friendlyTransportHelicopter.disableCooRemovedRadioMessage = true
         trigger.action.removeMark(briefingRoom.mission.features.friendlyTransportHelicopter.markID)
         briefingRoom.mission.features.friendlyTransportHelicopter.disableCooRemovedRadioMessage = false
       end
       briefingRoom.mission.features.friendlyTransportHelicopter.markID = event.idx
+      briefingRoom.mission.features.friendlyTransportHelicopter.goToCoordinates()
     elseif briefingRoom.mission.features.friendlyTransportHelicopter.markID ~= nil and event.idx == briefingRoom.mission.features.friendlyTransportHelicopter.markID then
-      briefingRoom.radioManager.play("Affirm, coordinates discarded. Awaiting new coordinates", "RadioCoordinatesDiscardedM")
+      briefingRoom.radioManager.play("Affirm, coordinates discarded. Awaiting new coordinates", "RadioCoordinatesDiscardedF")
       briefingRoom.mission.features.friendlyTransportHelicopter.markID = nil
     end
   end
@@ -58,10 +74,3 @@ end
 
 -- Add event handler
 world.addEventHandler(briefingRoom.mission.features.friendlyTransportHelicopter)
-
--- Create F10 menu options
-if briefingRoom.f10Menu.supportMenu == nil then
-  briefingRoom.f10Menu.supportMenu = missionCommands.addSubMenuForCoalition($PLAYERCOALITION$, "Support", nil)
-end
-missionCommands.addCommandForCoalition($PLAYERCOALITION$, "Transport helicopter, land on provided coordinates", briefingRoom.f10Menu.supportMenu, briefingRoom.mission.features.friendlyTransportHelicopter.goToCoordinates, nil)
-
